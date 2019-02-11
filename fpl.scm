ad819b3b8a696103c505522da2a6a1b219627128
George Jenkins <georgejenkins@Georges-MBP.fios-router.home>
2019-02-10 23:38:24 -0500

    added list reversal function

--- fpl.scm
+++ fpl.scm
@@ -2,3 +2,14 @@
 (define-namespace-anchor anc)
 (define ns (namespace-anchor->namespace anc))
 
+
+(define (reverse-general L)
+    (if (null? L)
+        L
+        (append (reverse-general (cdr L)) (list 
+         (if (not (list? (car L))) (car L)
+       (reverse-general (car L))
+    );if
+  )))
+  
+);
